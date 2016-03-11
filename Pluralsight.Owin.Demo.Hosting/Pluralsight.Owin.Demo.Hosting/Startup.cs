5b29f01d285177c332a53a91327662112ea94732
rifletcher <rifletcher@hotmail.com>
2016-03-11 15:36:42 +0200

    Initial commit

---
+++ Pluralsight.Owin.Demo.Hosting/Pluralsight.Owin.Demo.Hosting/Startup.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+using Owin;
+
+namespace Pluralsight.Owin.Demo.Hosting
+{
+    public class Startup
+    {
+        public static void Configuration(IAppBuilder app)
+        {
+            // view with http://localhost:12345/photo.jpg
+            app.UseStaticFiles();
+
+            app.Use(async (ctx, next) =>
+            {
+                await ctx.Response.WriteAsync("Hello World");
+            });
+        }
+    }
+}
---
+++ Pluralsight.Owin.Demo.Hosting/Pluralsight.Owin.Demo.Tests/OwinTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Microsoft.Owin.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Owin;
+using Pluralsight.Owin.Demo.Hosting;
+
+namespace Pluralsight.Owin.Demo.Tests
+{
+    [TestClass]
+    public class OwinTests
+    {
+        [TestMethod]
+        public async Task Owin_returns_200_on_reqest_to_root()
+        {
+            using (var server = TestServer.Create<Startup>())
+            {
+                var response = await server.HttpClient.GetAsync("/");
+                Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+            }
+        }
+
+        [TestMethod]
+        public async Task Owin_returns_hello_world_on_reqest_to_root()
+        {
+            using (var server = TestServer.Create<Startup>())
+            {
+                var response = await server.HttpClient.GetAsync("/");
+                var body = await response.Content.ReadAsStringAsync();
+                Assert.AreEqual("Hello World", body);
+            }
+        }
+
+        [TestMethod]
+        public async Task Owin_returns_correct_contenttype_on_reqest_to_jpg()
+        {
+            using (var server = TestServer.Create<Startup>())
+            {
+                var response = await server.HttpClient.GetAsync("/photo.jpg");
+                var contenttype = response.Content.Headers.ContentType.MediaType;
+                Assert.AreEqual("image/jpeg", contenttype);
+            }
+        }
+    }
+}
